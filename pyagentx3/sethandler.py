b621d0d569d0717f78f7e3fadf77b83a03f4eb21
Ernst de Vries <ernst.de.vries@gmail.com>
2025-08-02 19:28:02 +0000

    Simplified SetHandler

--- pyagentx3/sethandler.py
+++ pyagentx3/sethandler.py
@@ -20,42 +20,40 @@ class SetHandlerError(Exception):
 
 class SetHandler():
 
-    def __init__(self, data_store=None):
-        self.data_store = data_store
+    def __init__(self):
         self.transactions = {}
 
     def network_test(self, session_id, transaction_id, oid, data):
-        tid = "%s_%s" % (session_id, transaction_id)
-        if tid in self.transactions:
-            del self.transactions[tid]
+        tid = SetHandler._get_tid(session_id, transaction_id)
         try:
             self.test(oid, data)
             self.transactions[tid] = oid, data
         except SetHandlerError as e:
+            self.transactions.pop(tid)
             logger.error('TestSet failed: %s', e)
             raise e
 
     def network_commit(self, session_id, transaction_id):
-        tid = "%s_%s" % (session_id, transaction_id)
-        if tid not in self.transactions:
-            return
+        tid = SetHandler._get_tid(session_id, transaction_id)
         try:
-            oid, data = self.transactions[tid]
+            oid, data = self.transactions.get(tid, (None, None))
+            if oid is None:
+                return
             self.commit(oid, data)
-            if tid in self.transactions:
-                del self.transactions[tid]
+            self.transactions.pop(tid)
         except Exception as e:
             logger.error('CommitSet failed: %s', e)
 
     def network_undo(self, session_id, transaction_id):
-        tid = "%s_%s" % (session_id, transaction_id)
-        if tid in self.transactions:
-            del self.transactions[tid]
+        self.network_cleanup(session_id, transaction_id)
 
     def network_cleanup(self, session_id, transaction_id):
-        tid = "%s_%s" % (session_id, transaction_id)
-        if tid in self.transactions:
-            del self.transactions[tid]
+        tid = SetHandler._get_tid(session_id, transaction_id)
+        self.transactions.pop(tid)
+
+    @staticmethod
+    def _get_tid(session_id, transaction_id):
+        return f"{session_id}_{transaction_id}"
 
     # User override these
     def test(self, oid, data):
